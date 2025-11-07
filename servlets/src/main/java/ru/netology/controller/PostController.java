a08fb480fee4bfa167f7a30ee979199ac0e26c08
Zhi An <zhidkovaayu@gmail.com>
2025-11-08 00:05:49 +0400

    Initial commit

---
+++ servlets/src/main/java/ru/netology/controller/PostController.java
@@ -0,0 +1,54 @@
+package ru.netology.controller;
+
+import com.google.gson.Gson;
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+import ru.netology.service.PostService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Reader;
+
+public class PostController {
+  public static final String APPLICATION_JSON = "application/json";
+  private final PostService service;
+
+  public PostController(PostService service) {
+    this.service = service;
+  }
+
+  public void all(HttpServletResponse response) throws IOException {
+    response.setContentType(APPLICATION_JSON);
+    final var data = service.all();
+    final var gson = new Gson();
+    response.getWriter().print(gson.toJson(data));
+  }
+
+  public void getById(long id, HttpServletResponse response) throws IOException {
+    try {
+      final var post = service.getById(id);
+      response.setContentType(APPLICATION_JSON);
+      final var gson = new Gson();
+      response.getWriter().print(gson.toJson(post));
+    } catch (NotFoundException e) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+  }
+
+  public void save(Reader body, HttpServletResponse response) throws IOException {
+    response.setContentType(APPLICATION_JSON);
+    final var gson = new Gson();
+    final var post = gson.fromJson(body, Post.class);
+    final var data = service.save(post);
+    response.getWriter().print(gson.toJson(data));
+  }
+
+  public void removeById(long id, HttpServletResponse response) throws IOException {
+    try {
+      service.removeById(id);
+      response.setStatus(HttpServletResponse.SC_NO_CONTENT); // 204 — успешно, но без тела
+    } catch (NotFoundException e) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+  }
+}
